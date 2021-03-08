b5750154d8e340953b950b7ce91f9850ecdaf75b
OlegSlav <78816060+OlegSlav@users.noreply.github.com>
2021-03-08 16:33:59 +0300

    Add files via upload

---
+++ lesson12.cpp
@@ -0,0 +1,31 @@
+﻿// lesson12.cpp 
+//Задание 2. Пользователь вводит параметры фигуры. Найти периметр и площадь фигур:f. Салинон
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+#include <fstream>
+
+void Filewrite(float S, float P);
+
+int main()
+{   
+    float r1, r2;
+    std::cout << "Please enter values of radiuses r1 and r2\nto find Square and Perimeter of Salinon:";
+    std::cin >> r1 >> r2;
+    float S = .25 * M_PI * (r1 + r2) * (r1 + r2);
+    float P = .5 * S;          //не нашел формулу
+    std::cout << "The Square is:" << S << "\nThe Perimeter is:" << P << std::endl;
+    Filewrite(S,P);
+    std::getchar();
+    std::getchar();
+
+    return 0;
+}
+
+void Filewrite(float S, float P) {
+    std::ofstream inFile;
+    inFile.open("Answer2.txt");
+    inFile << "The Square is:" << S << "\nThe Perimeter is:" << P << std::endl;
+    inFile.close();
+}
