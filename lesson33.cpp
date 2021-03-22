b069c89f39eca682f6f43cad02b2276180f42624
OlegSlav <78816060+OlegSlav@users.noreply.github.com>
2021-03-22 15:03:56 +0300

    Add files via upload

---
+++ lesson33.cpp
@@ -0,0 +1,42 @@
+﻿// lesson33.cpp : Задание 3, задача 1-I
+//y = 2x^2 - 3, если x > 7
+//y = 0, если x = 7
+//y = 2 * | x | +3, если x < 7
+
+#define USE_MATH_DEFINES
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+void Filewrite(float y);
+
+int main()
+{   
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Ведите x:\n";
+    float x, y;
+    std::cin >> x;
+    if (x > 7) {
+        y = 2 * pow(x, 2) - 3;
+    }
+    else {
+        if (x < 7) {
+            y = 2 * abs(x) + 3;
+        }
+        else {
+            y = 0;
+        }
+    }
+    std::cout << "y=" << y << std::endl;
+    Filewrite(y);
+    std::getchar();
+    std::getchar();
+    return 0;
+}
+
+void Filewrite(float y){
+    std::ofstream inFile;
+    inFile.open("Answer33");
+    inFile << "y=" << y << std::endl;
+    inFile.close();
+}
---
+++ lesson34.cpp
@@ -0,0 +1,27 @@
+﻿// lesson34.cpp :Задание 4, задача 2е.
+//Напишите программу, которая принимает от пользователя координаты точки и
+//определяет, попала ли точка в заштрихованную область.
+
+#define USE_MATH_DEFINES
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите x и y:\n";
+    float x, y;
+    std::cin >> x >> y;
+    if ((sqrt(x * x + y * y) <= 1) && ((y >= -x) || ((y <= -x) && (x >= 0)))) {
+        std::cout << " Yeah, got it!";
+    }
+    else {
+        std::cout << "Nope";
+    };
+    std::getchar();
+    std::getchar();
+    return 0;
+
+}
+
